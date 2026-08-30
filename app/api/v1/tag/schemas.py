0ceb2d5249a6ef584bc99421b58f038f67279c20
jonanv <jonan-vargas23@hotmail.com>
2026-08-30 08:59:45 -0500

    ✨ Add TagCreate to schemas of tag

--- app/api/v1/tag/schemas.py
+++ app/api/v1/tag/schemas.py
@@ -9,4 +9,13 @@ class TagPublic(BaseModel):
         examples=["Esta es mi primer etiqueta"]
     )
     
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True) # sirve para permitir que un modelo lea datos directamente de los atributos de un objeto normal (como una instancia de una base de datos o un ORM como SQLAlchemy)
+    
+class TagCreate(BaseModel):
+    name: str = Field(
+        ...,
+        min_length=3,
+        max_length=30,
+        description="Nombre de la etiquetas",
+        examples=["Esta es mi primer etiqueta"]
+    )
