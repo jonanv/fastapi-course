9eddca1d98cbed84c6be7dc1beb7f51b031462be
jonanv <jonan-vargas23@hotmail.com>
2026-09-01 08:58:36 -0500

    ✨ Create reposotory for tag method get

---
+++ app/api/v1/tag/repository.py
@@ -0,0 +1,15 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models.tag import TagORM
+
+class TagRepository:
+    def __init__(self, db: Session):
+        self.db = db
+        
+    def get(self, tag_id: int):
+        tag_find = (
+            select(TagORM)
+            .where(TagORM.id == tag_id)
+        )
+        return self.db.execute(tag_find).scalar_one_or_none()
