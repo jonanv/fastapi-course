b7c4c6bfc0608f8da05b43bc4bcd5ec00021e6b7
jonanv <jonan-vargas23@hotmail.com>
2026-09-01 11:30:36 -0500

    ✨ Create router into router for create_tag

--- app/api/v1/tag/router.py
+++ app/api/v1/tag/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, Path, HTTPException, status
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 from app.api.v1.tag.repository import TagRepository
-from app.api.v1.tag.schemas import TagPublic
+from app.api.v1.tag.schemas import TagCreate, TagPublic
 from app.core.db import get_db
+from app.models.tag import TagORM
 
 router = APIRouter(prefix="/tags", tags=["tag"])
 
@@ -25,3 +27,24 @@ def get_tag(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag no encontrado")
     
     return TagPublic.model_validate(tag, from_attributes=True)
+
+@router.post("", response_model=TagCreate, response_description="Tag creada (OK)", status_code=status.HTTP_201_CREATED)
+def create_tag(
+    tag: TagCreate,
+    db: Session = Depends(get_db)
+) -> TagORM:
+    repository =  TagRepository(db)
+    
+    try:
+        new_tag = repository.create_tag(
+            name=tag.name
+        )
+        db.commit()
+        db.refresh(new_tag)
+        return new_tag
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="El nombre de la etiqueta ya existe")
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error al crear el tag")
