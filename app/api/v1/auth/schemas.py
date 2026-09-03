e462e9b6fa0082764f0a08565f58573bb0cda97a
jonanv <jonan-vargas23@hotmail.com>
2026-09-03 08:42:51 -0500

    🎨 Add schemas to schemas of user

--- app/api/v1/auth/schemas.py
+++ app/api/v1/auth/schemas.py
@@ -1,35 +1,6 @@
-from typing import Literal, Optional
+from pydantic import BaseModel
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
-
-Role = Literal["user", "editor", "admin"]
-
-class UserBase(BaseModel):
-    email: EmailStr
-    full_name: Optional[str] = None
-    
-    model_config = ConfigDict(from_attributes=True)
-    
-class UserPublic(UserBase):
-    id: int
-    role: Role
-    is_active: bool
-    
-class UserCreate(UserBase):
-    email: EmailStr
-    password: str = Field(
-        ...,
-        min_length=6,
-        max_length=100
-    )
-    full_name: Optional[str] = None
-
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-    
-class RoleUpdate(BaseModel):
-    role: Role
+from ..user.schemas import UserPublic
     
 class TokenResponse(BaseModel):
     access_token: str
