fc6589e6a96d757db14f11e9f0d1f23d6733a039
jonanv <jonan-vargas23@hotmail.com>
2026-09-04 21:01:46 -0500

    🔥 Delete model Author and create relationship with User

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,13 @@
 from datetime import datetime
-from typing import Literal
+from typing import TYPE_CHECKING, Literal, List
 
 from sqlalchemy import Boolean, DateTime, Integer, String, UniqueConstraint, Enum
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core.db import Base
+
+if TYPE_CHECKING:
+    from .post import PostORM
 
 Role = Literal["user", "editor", "admin"]
 
@@ -20,3 +23,5 @@ class UserORM(Base):
     role: Mapped[Role] = mapped_column(Enum("user", "editor", "admin", name="rol_enum"), default="user")
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    
+    posts: Mapped[List["PostORM"]] = relationship(back_populates="user")
