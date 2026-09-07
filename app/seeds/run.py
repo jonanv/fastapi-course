4a63ba8fb9e66a06ea881296f13ac05cef962e10
jonanv <jonan-vargas23@hotmail.com>
2026-09-07 14:42:29 -0500

    ✨ Create commands for run seeds

---
+++ app/seeds/run.py
@@ -0,0 +1,26 @@
+import typer
+
+from app.seeds.services import run_users, run_categories, run_tags, run_all_seeds
+
+app = typer.Typer(help="Seeds: users, categories, tags")
+
+
+@app.command("users")
+def users():
+    run_users()
+    typer.echo("Usuarios cargados")
+
+@app.command("categories")
+def categories():
+    run_categories()
+    typer.echo("Categorías cargadas")
+
+@app.command("tags")
+def tags():
+    run_tags()
+    typer.echo("Etiquetas cargados")
+
+@app.command("users")
+def all_seeds():
+    run_all_seeds()
+    typer.echo("Todos los seeds fueron cargados")
