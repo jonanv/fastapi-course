30ef95358e279d9924810d5ea57210cb38694638
jonanv <jonan-vargas23@hotmail.com>
2026-09-09 15:05:59 -0500

    🔧 Add config for detault database and create conection

--- devinote/app/core/config.py
+++ devinote/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = Field(..., env="DATABASE_URL")
+    DATABASE_URL: str = Field(..., default="sqlite:///./devinote/devinote.db", env="DATABASE_URL") # Si no se proporciona DATABASE_URL en el entorno, se usará SQLite como base de datos predeterminada.
     JWT_SECRET_KEY: str = Field(..., env="JWT_SECRET_KEY")
     JWT_ALGORITHM: str = Field(default="HS256", env="JWT_ALGORITHM")
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=60*24, env="JWT_ACCESS_TOKEN_EXPIRE_MINUTES")
