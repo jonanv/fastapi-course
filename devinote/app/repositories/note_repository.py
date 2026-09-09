4eab8bafa48d6fdb98b4f479aad33672f1f6f5e5
jonanv <jonan-vargas23@hotmail.com>
2026-09-09 18:08:54 -0500

    ✨ Add repository for note

---
+++ devinote/app/repositories/note_repository.py
@@ -0,0 +1,56 @@
+from sqlalchemy import delete
+from sqlmodel import Session, select
+
+from devinote.app.models.label import NoteLabelLink
+from devinote.app.models.note import Note
+
+
+class NoteRepository:
+    def __init__(self, db: Session):
+        self.db = db
+    
+    def list_owned(self, owned_id: int) -> list[Note]:
+        query = (
+            select(Note)
+            .where(Note.owner_id == owned_id)
+            .order_by(Note.id.desc())
+        )
+        return self.db.exec(query).all()
+    
+    def get_by_id(self, note_id: int) -> Note | None:
+        return self.db.get(Note, note_id)
+    
+    def create_note(self, note: Note) -> Note:
+        self.db.add(note)
+        self.db.flush()
+        self.db.commit()
+        self.db.refresh(note)
+        return note
+    
+    def update_note(self, note: Note) -> Note:
+        self.db.add(note)
+        self.db.flush()
+        self.db.commit()
+        self.db.refresh(note)
+        return note
+    
+    def delete_note(self, note: Note) -> None:
+        query = (
+            delete(NoteLabelLink)
+            .where(NoteLabelLink.note_id == note.id)
+        )
+        self.db.exec(query)
+        self.db.delete(note)
+        self.db.commit()
+    
+    def replace_labels(self, owned_id: int, note_id: int, label_ids: list[int]) -> None:
+        query = (
+            delete(NoteLabelLink)
+            .where(NoteLabelLink.note_id == note_id)
+        )
+        self.db.exec(query)
+        
+        for label_id in set(label_ids or []):
+            self.db.add(NoteLabelLink(note_id=note_id, label_id=label_id))
+        
+        self.db.commit()
