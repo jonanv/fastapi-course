20cf75f8ec83615518dd9bed57c84814fef92a53
jonanv <jonan-vargas23@hotmail.com>
2026-09-10 18:14:05 -0500

    ✨ Add service for note

---
+++ devinote/app/services/note_service.py
@@ -0,0 +1,38 @@
+
+
+from sqlmodel import Session
+
+from ..models.share import ShareRole
+from ..models.note import Note
+from ..repositories.share_repository import ShareRepository
+from ..repositories.label_repository import LabelRepository
+from ..repositories.note_repository import NoteRepository
+
+
+class NoteService:
+    def __init__(self, db: Session):
+        self.db = db
+        self.notes = NoteRepository(db)
+        self.labels = LabelRepository(db)
+        self.shares = ShareRepository(db)
+    
+    # Permisos
+    def user_can_read(self, user_id: int, note: Note) -> bool:
+        if note.owner_id == user_id:
+            return True
+
+        if self.shares.has_note_share(note_id=note.id, user_id=user_id):
+            return True
+        
+        label_ids = self.labels.list_label_ids_for_note(note.id)
+        return self.shares.has_any_label_share(label_ids=label_ids, user_id=user_id)
+    
+    def user_can_edit(self, user_id: int, note: Note) -> bool:
+        if note.owner_id == user_id:
+            return True
+        
+        if self.shares.has_note_share(note_id=note.id, user_id=user_id, role=ShareRole.EDIT):
+            return True
+        
+        label_ids = self.labels.list_label_ids_for_note(note.id)
+        return self.shares.has_any_label_share(label_ids=label_ids, user_id=user_id, role=ShareRole.EDIT)
