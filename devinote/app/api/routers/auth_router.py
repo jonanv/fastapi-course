aa3bc601c0920b53e51aed1ae222b61c93d7ca30
jonanv <jonan-vargas23@hotmail.com>
2026-09-11 12:13:52 -0500

    ✨ Create auth router and methos register, login and token

---
+++ devinote/app/api/routers/auth_router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from ...repositories.user_repository import UserRepository
+from ...services.auth_service import AuthService
+from ...api.dependencies import DBSession
+from ...models.user import UserCreate, UserRead
+
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.post("/register", response_model=UserRead, response_description="Registrar un nuevo usuario", status_code=status.HTTP_201_CREATED)
+def register(payload: UserCreate, db: DBSession) -> UserCreate:
+    service = AuthService(UserRepository(db))
+    return service.register(payload)
+
+@router.post("/login", response_model=UserRead, response_description="", status_code=status.HTTP_200_OK)
+def login(email: str, password: str, db: DBSession) -> str:
+    service = AuthService(UserRepository(db))
+    token = service.login(email, password)
+    return { "token": token, "token_type": "bearer" }
+
+@router.post("/token", response_model=UserRead, response_description="", status_code=status.HTTP_200_OK)
+def token(db: DBSession, form: OAuth2PasswordRequestForm = Depends()) -> str:
+    email = form.username
+    password = form.password
+    service = AuthService(UserRepository(db))
+    token = service.login(email, password)
+    return { "token": token, "token_type": "bearer" }
