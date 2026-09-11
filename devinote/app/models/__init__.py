c56d84945e797a6b1481d71506e6ede6969e5d3b
jonanv <jonan-vargas23@hotmail.com>
2026-09-11 15:32:18 -0500

    ✨ Import into __init__.py model

---
+++ devinote/app/models/__init__.py
@@ -0,0 +1,13 @@
+from .note import Note
+from .label import Label, NoteLabelLink
+from .share import NoteShare, LabelShare
+from .user import User
+
+__all__ = [
+    "Note",
+    "Label",
+    "NoteLabelLink",
+    "NoteShare",
+    "LabelShare",
+    "User"
+]
