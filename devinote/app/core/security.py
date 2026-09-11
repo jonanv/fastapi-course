c5768aa94e50821b26bb6d959f22344ae02e6ca1
jonanv <jonan-vargas23@hotmail.com>
2026-09-11 17:55:59 -0500

    🐛 Fix error with jwt encode into security

--- devinote/app/core/security.py
+++ devinote/app/core/security.py
@@ -20,7 +20,7 @@ def create_access_token(data: dict, minutes: int | None = None) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=minutes or settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({ "exp": expire })
-    return jwt.enconde(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
 
 def decode_token(token: str) -> dict:
     """Decodifica un token JWT y devuelve su contenido como un diccionario."""
