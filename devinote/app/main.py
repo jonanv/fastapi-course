25518edb55052c273ed506ff4f3e6b92609e96fb
jonanv <jonan-vargas23@hotmail.com>
2026-09-11 14:08:57 -0500

    ✨ Import router and create middleware to main

--- devinote/app/main.py
+++ devinote/app/main.py
@@ -1,8 +1,24 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+
+from ..app.db.database import init_db
+from ..app.api.routers import auth_router, label_router, note_router, share_router
+from ..app.core.config import settings
+
+
+load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
 
 def create_app() -> FastAPI:
     app = FastAPI(
-        title="FastAPI Application", 
+        title=settings.PROJECT_NAME,
+        lifespan=lifespan,
         description="This is a sample FastAPI application.", 
         version="1.0.0",
         swagger_ui_parameters={
@@ -10,9 +26,18 @@ def create_app() -> FastAPI:
         }
     )
     
-    @app.get("/")
-    def read_root():
-        return { "message": "Hello World" }
+    app.middleware(
+        CORSMiddleware,
+        allow_origins=["*"],    # Permitir todos los origines
+        allow_credentials=True, # Permitir credenciales
+        allow_methods=["*"],    # Permitir todos los métodos
+        allow_headers=["*"]     # Permitir todos los headers
+    )
+    
+    app.include_router(auth_router, prefix="/api/v1")
+    app.include_router(note_router, prefix="/api/v1")
+    app.include_router(label_router, prefix="/api/v1")
+    app.include_router(share_router, prefix="/api/v1")
     
     return app
 
