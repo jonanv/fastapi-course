790659d8eab401c0b77790093eb85e6b5fbb3c56
jonanv <jonan-vargas23@hotmail.com>
2026-09-11 13:36:01 -0500

    ✨ Add new model ShareRequest to share model

--- devinote/app/models/share.py
+++ devinote/app/models/share.py
@@ -25,3 +25,7 @@ class LabelShare(SQLModel, table=True):
     label_id: int = Field(foreign_key="label.id", index=True)
     user_id: int = Field(foreign_key="user.id", index=True)
     role: ShareRole = Field(default=ShareRole.READ)
+
+class ShareRequest(SQLModel):
+    target_user_id: int = Field(ge=1)
+    role: ShareRole = ShareRole.READ
