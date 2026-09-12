b28431a505dded2ddf0dd9a92a76ca1cb837d546
jonanv <jonan-vargas23@hotmail.com>
2026-09-12 12:11:15 -0500

    🔧 Config alembic with sqlmodel and database_url

--- devinote/alembic/env.py
+++ devinote/alembic/env.py
@@ -1,9 +1,17 @@
 from logging.config import fileConfig
+import os
+from dotenv import load_dotenv
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
+from sqlmodel import SQLModel
+
+from app.models.user import User
+from app.models.label import Label, NoteLabelLink
+from app.models.note import Note
+from app.models.share import NoteShare, LabelShare
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +26,11 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,11 +50,13 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = DATABASE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
+        compare_type=True,      # Detecta los cambios de los tipos
+        compare_server_default=True,
         dialect_opts={"paramstyle": "named"},
     )
 
@@ -57,15 +71,18 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+    connectable = create_engine(
+        DATABASE_URL,
         poolclass=pool.NullPool,
+        future=True
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            compare_server_default=True
         )
 
         with context.begin_transaction():
